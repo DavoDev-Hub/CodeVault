b40c0f80eeafca735cbb9446f519385d9fe2578d
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-18 17:18:15 -0600

    ex-3-9

--- Books/Python/PythonCrashCourse/lists/ex3-9.py
+++ Books/Python/PythonCrashCourse/lists/ex3-9.py
@@ -1,7 +1,32 @@
 list = ['Mexico', 'Japan', 'Spain', 'Brazil', 'Italy']
 print(list)
 
-print(sorted(list))
+print("sorted list")
+sorted_list = sorted(list)
+print(sorted_list)
 
+print("Original list")
+print(list)
 
-print(sorted(list).reverse())
+print("Sorted list reverse")
+sorted_reverse_list = sorted(list)
+sorted_reverse_list.reverse()
+print(sorted_reverse_list)
+print("Original list")
+print(list)
+
+print("reverse list")
+list.reverse()
+print(list)
+list.reverse()
+print("list reversed back")
+list.reverse()
+print(list)
+
+print("Sort")
+list.sort()
+print(list)
+
+print("list sort reverse")
+list.sort(reverse=True)
+print(list)
