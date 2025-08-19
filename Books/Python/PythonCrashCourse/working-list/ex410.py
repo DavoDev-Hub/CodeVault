ed30464dfdcf41eeef40dcc180ebd9dc28861b9a
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-19 07:30:54 -0600

    ex 10

---
+++ Books/Python/PythonCrashCourse/working-list/ex410.py
@@ -0,0 +1,5 @@
+students = ["Alonso", "Daniel", "Oscar", "Pablo", "Angel"]
+print(f"List: {students}")
+print(f"The first three items in the list are: {students[:3]}")
+print(f"The three items from the middle of the list are: {students[1:-1]} ")
+print(f"The three last items in the list are: {students[2:]}")
