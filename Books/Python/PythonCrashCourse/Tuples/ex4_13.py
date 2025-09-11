b37ce8c4ea7b6b50f27a60769b8ac02b2cfd4617
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-10 18:23:31 -0600

    Agregar ejemplo de tuplas con alimentos en ex4_13.py

---
+++ Books/Python/PythonCrashCourse/Tuples/ex4_13.py
@@ -0,0 +1,13 @@
+print('Food: ')
+foods = ('Pizza', 'Pasta', 'Tacos', 'Cereal', 'Eggs')
+
+for food in foods:
+    print(food)
+
+print('\n')
+# foods[0] = 'Kebab'
+
+print('New food: ')
+foods = ('Pizza', 'Pasta', 'Tacos', 'Kebab', 'Beacon')
+for food in foods:
+    print(food)
