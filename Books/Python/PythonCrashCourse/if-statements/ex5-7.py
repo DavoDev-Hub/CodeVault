7df55bca5481321391365b155e54da402d7f1f25
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-11 18:49:09 -0600

    Agregar archivo ex5-7.py con ejemplos de frutas favoritas y condiciones

---
+++ Books/Python/PythonCrashCourse/if-statements/ex5-7.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['apple', 'orange', 'banana']
+
+if 'apple' in favorite_fruits:
+    print('You really like apples')
+
+
+if 'orange' in favorite_fruits:
+    print('You really like oranges')
+
+
+if 'banana' in favorite_fruits:
+    print('You really like banana')
