c476bec0fe0c5b5fce7da5b62a7694d12666c087
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-15 18:37:52 -0600

    Agregar archivo ex6-1.py con un ejemplo de uso de diccionarios para almacenar información de un amigo

---
+++ Dictionary/ex6-1.py
@@ -0,0 +1,9 @@
+my_friend = {
+    "first_name" : "Linus", 
+    "last_name" : "Torvalds",
+    "city" : "Finlandia"
+}
+
+print(f"My best friend is {my_friend["first_name"]}")
+print(f"His last name is {my_friend["last_name"]}")
+print(f"He's from {my_friend["city"]}")
