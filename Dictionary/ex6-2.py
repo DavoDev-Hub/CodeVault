4b44c1bdcce1be5096321a643903bb077a8d9440
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-15 18:37:56 -0600

    Agregar archivo ex6-2.py con un ejemplo de uso de diccionarios para almacenar y mostrar números favoritos de amigos

---
+++ Dictionary/ex6-2.py
@@ -0,0 +1,6 @@
+friends_numbers = {"Alex" : 13, "Andre": 10, "Jaz":7}
+
+print("Favourite numbers")
+print(f"Alex: {friends_numbers["Alex"]}")
+print(f"Andre: {friends_numbers["Andre"]}")
+print(f"Jaz: {friends_numbers["Jaz"]}")
