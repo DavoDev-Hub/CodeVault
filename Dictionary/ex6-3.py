1d9914cbba3180a44136f2baae01caf5412608b3
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-15 18:38:09 -0600

    Agregar archivo ex6-3.py con un ejemplo de uso de diccionarios para almacenar y mostrar significados

---
+++ Dictionary/ex6-3.py
@@ -0,0 +1,13 @@
+glossary = {
+    "Python": "Is a programming languague",
+    "DSA": "Data Structure and Algorithms",
+    "Rat": "Alex is a rat"
+}
+
+python_meaning = glossary["Python"]
+dsa = glossary["DSA"]
+alex = glossary["Rat"]
+
+print(f"Python:  {python_meaning} \n")
+print(f"DSA:  {dsa} \n")
+print(f"Alex:  {alex} \n")
