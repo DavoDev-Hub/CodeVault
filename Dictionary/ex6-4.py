167c90518729f7cc0e184ea6782f6462eda6ca6f
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-19 19:48:14 -0600

    Agregar archivo ex6-4.py con un ejemplo de uso de diccionarios para almacenar y mostrar definiciones

---
+++ Dictionary/ex6-4.py
@@ -0,0 +1,13 @@
+glossary = {
+    "Python": "Is a programming languague",
+    "DSA": "Data Structure and Algorithms",
+    "Rat": "Alex is a rat",
+    "Issue": "My life"
+}
+
+
+
+
+for k,v in glossary.items():
+    print(f"{k}: {v}") 
+
