94ac599728f1dcd9704e46b32f78e97378044757
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-22 18:45:28 -0600

    Agregar archivo ex6-7.py con ejemplos de uso de diccionarios y eliminar archivo ex6-8.py vacío

---
+++ Books/Python/PythonCrashCourse/Dictionary/ex6-7.py
@@ -0,0 +1,23 @@
+my_friend = {
+    "first_name" : "Linus", 
+    "last_name" : "Torvalds",
+    "city" : "Finlandia"
+}
+
+
+my_enemy = {
+    "first_name" : "Steve", 
+    "last_name" : "Jobs",
+    "city" : "San Francisco"
+}
+
+my_god = {
+    "first_name" : "Terry", 
+    "last_name" : "Davis",
+    "city" : "Wisconsin"
+}
+
+people = [my_friend, my_enemy, my_god]
+
+for person in people:
+    print(person)
