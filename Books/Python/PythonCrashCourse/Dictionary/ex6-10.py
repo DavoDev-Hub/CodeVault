22c844a13f8ffa9bf2e97e5ba88f690494ecea55
DavoDev-Hub <juanpajmz13@gmail.com>
2025-09-23 18:16:32 -0600

    Agregar ejemplos de uso de diccionarios en los archivos ex6-9.py y ex6-10.py, y crear archivo ex6-11.py vacío para futuras implementaciones

---
+++ Books/Python/PythonCrashCourse/Dictionary/ex6-10.py
@@ -0,0 +1,8 @@
+friends_numbers = {
+        "Alex" : [13, 14], 
+        "Andre": [10,11], 
+        "Jaz":[7,8]
+}
+
+for k, v in friends_numbers.items():
+    print(f'{k}: {v}')
---
+++ Books/Python/PythonCrashCourse/Dictionary/ex6-9.py
@@ -0,0 +1,10 @@
+favorite_places = {
+    'Steve': ['Spain', 'Mexico', 'Japan'],
+    'Mark Zuckerberg': ['Italy', 'Greece', 'Turkey'],
+    'Bill Gates': ['France', 'Germany', 'Switzerland'],
+    'Elon Musk': ['Canada', 'Australia', 'New Zealand'],
+}
+
+print('Favorite places')
+for k, v in favorite_places.items():
+    print(f"{k}: {v} ")
