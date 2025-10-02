d53018e71b2790a6d30ac441820690609b27053f
DavoDev-Hub <juanpajmz13@gmail.com>
2025-10-01 18:14:34 -0600

    Agregar archivos ex8-5.py y ex8-6.py con funciones para describir ciudades y países.

---
+++ Books/Python/PythonCrashCourse/functions/ex8-6.py
@@ -0,0 +1,6 @@
+def city_country(city, country):
+    print(f'{city.title()}, {country.title()}')
+
+city_country('Santiago', 'Chile')
+city_country('CDMX', 'Mexico')
+city_country('Bogotá', 'Colombia')
