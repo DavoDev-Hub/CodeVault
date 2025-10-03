86ab9152b951697f7de735a8380bc4d415f925a4
DavoDev-Hub <juanpajmz13@gmail.com>
2025-10-02 19:07:16 -0600

    Refactor uso de make_car en ex8-15.py para mejorar la claridad del código

--- Books/Python/PythonCrashCourse/functions/ex8-15.py
+++ Books/Python/PythonCrashCourse/functions/ex8-15.py
@@ -1,4 +1,8 @@
+import module
 from module import make_car
+from module import make_car as mc
+import module as md
+from module import *
 
-car = make_car('subaru', 'outback', color='blue', tow_package=True)
+car = mc('subaru', 'outback', color='blue', tow_package=True)
 print(car)
