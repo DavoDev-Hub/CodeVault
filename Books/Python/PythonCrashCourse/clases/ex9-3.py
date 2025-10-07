38fb7138ba483c4175d504a446632c67b97d26e7
DavoDev-Hub <juanpajmz13@gmail.com>
2025-10-06 21:02:45 -0600

    Agregar clase User con métodos para describir y saludar al usuario

---
+++ Books/Python/PythonCrashCourse/clases/ex9-3.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def describe_user(self):
+        print(f'Name: {self.first_name} {self.last_name}, {self.age}')
+
+    def greet_user(self):
+        print(f'Hello {self.first_name} good day!!!')
+    
+
+user = User('Juan Pablo', 'Jimenez', '21')
+
+user.describe_user()
+user.greet_user()
+
+user = User('Negrox', 'Rodriguez', '22')
+user.describe_user()
+user.greet_user()
+
