f52b31ff251ac406603953414029ce08cb44a861
DavoDev-Hub <juanpajmz13@gmail.com>
2026-01-07 18:40:50 -0600

    Agregar script para leer el número favorito del usuario desde un archivo JSON

---
+++ Books/Python/PythonCrashCourse/files_and_exceptions/ex10-11.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import json
+
+path = Path('Books/Python/PythonCrashCourse/files_and_exceptions/username.json')
+
+'''number = input("What's your favorite number? ")
+
+contents = json.dumps(number)
+path.write_text(contents)
+'''
+
+contents = path.read_text()
+number = json.loads(contents)
+print(f'I know your favorite number is ', number )
