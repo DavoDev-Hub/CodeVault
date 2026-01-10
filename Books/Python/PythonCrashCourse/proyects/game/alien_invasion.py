247ffacb5d4424d9a1cf06b595f73078cd4633a9
DavoDev-Hub <juanpajmz13@gmail.com>
2026-01-09 19:27:21 -0600

    Agregar comentarios para mejorar la legibilidad del código en la clase AlienInvasion

--- Books/Python/PythonCrashCourse/proyects/game/alien_invasion.py
+++ Books/Python/PythonCrashCourse/proyects/game/alien_invasion.py
@@ -8,14 +8,14 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((1200, 800)) # Set the screen size
+        pygame.display.set_caption("Alien Invasion") # Set the window title
 
     def run_game(self):
         """Start the main loop for the game."""
-        while True:
+        while True: # Main loop for the game
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
+            for event in pygame.event.get(): # Event loop 
                 if event.type == pygame.QUIT:
                     sys.exit()
             # Make the most recently drawn screen visible.
