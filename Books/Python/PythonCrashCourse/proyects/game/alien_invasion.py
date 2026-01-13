f15983419b5b9b7031954b79389f270a730517d2
DavoDev-Hub <juanpajmz13@gmail.com>
2026-01-12 19:49:50 -0600

    Agregar actualización de posición de la nave y gestión de eventos de teclado en la clase AlienInvasion; ajustar color de fondo en la clase Settings.

--- Books/Python/PythonCrashCourse/proyects/game/alien_invasion.py
+++ Books/Python/PythonCrashCourse/proyects/game/alien_invasion.py
@@ -22,6 +22,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True: # Main loop for the game
             self._check_events()
+            self.ship.update()  # Update the ship's position
             self.update_screen()
             self.clock.tick(60)  # Limit the frame rate to 60 frames per second
 
@@ -31,6 +32,12 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def update_screen(self):
         """Update images on the screen, and flip to the new screen."""
