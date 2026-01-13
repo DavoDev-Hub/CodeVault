4315cc4dc30f4190c45c2485143c6fb90d5d48c3
DavoDev-Hub <juanpajmz13@gmail.com>
2026-01-12 19:24:13 -0600

    Refactor la clase AlienInvasion para mejorar la gestión de eventos y la actualización de la pantalla; corrige la ruta de la imagen de la nave en la clase Ship.

--- Books/Python/PythonCrashCourse/proyects/game/alien_invasion.py
+++ Books/Python/PythonCrashCourse/proyects/game/alien_invasion.py
@@ -11,32 +11,35 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock() # Initialize the game clock 
         self.settings = Settings()
-
-
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height)) # Set the screen size
+        
         pygame.display.set_caption("Alien Invasion") # Set the window title
-        self.ship = Ship(self.screen)
-
+        self.ship = Ship(self) # Create an instance of Ship
         # Set the background color.
         self.bg_color = self.settings.bg_color
 
     def run_game(self):
         """Start the main loop for the game."""
         while True: # Main loop for the game
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get(): # Event loop 
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
-            self.ship.blitme()
-            
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self.update_screen()
             self.clock.tick(60)  # Limit the frame rate to 60 frames per second
 
-            
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.bg_color) # Fill the screen with the background color
+        self.ship.blitme() # Draw the ship on the screen
+        
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
--- Books/Python/PythonCrashCourse/proyects/game/ship.py
+++ Books/Python/PythonCrashCourse/proyects/game/ship.py
@@ -9,7 +9,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('Books/Python/PythonCrashCourse/proyects/game/images/ship.bmp')
         self.rect = self.image.get_rect() # Get the rectangular area of the image
 
         # Start each new ship at the bottom center of the screen.
