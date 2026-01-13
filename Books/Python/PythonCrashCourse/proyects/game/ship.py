506fb112e40f77afc0cf9ea48a23e07cba147444
DavoDev-Hub <juanpajmz13@gmail.com>
2026-01-12 20:02:05 -0600

    Agregar gestión de movimiento de la nave hacia la izquierda; actualizar la clase Ship para incluir velocidad y posición horizontal.

--- Books/Python/PythonCrashCourse/proyects/game/ship.py
+++ Books/Python/PythonCrashCourse/proyects/game/ship.py
@@ -7,6 +7,7 @@ class Ship:
         """ Initialize the ship and set its starting position. """
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('Books/Python/PythonCrashCourse/proyects/game/images/ship.bmp')
@@ -15,14 +16,23 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # store a float for the ship's horizontal position
+        self.x = float(self.rect.x)
+
+
         # Movement flag; start with a ship that is not moving.
         self.moving_right = False
+        self.moving_left = False
     
     def update(self):
         """ Update the ship's position based on movement flags. """
         if self.moving_right:
-            self.rect.x += 1  # Move the ship to the right
+            self.x += self.settings.ship_speed  # Move the ship to the right
+        if self.moving_left:
+            self.x -= self.settings.ship_speed  # Move the ship to the left
 
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """ Draw the ship at its current location. """
