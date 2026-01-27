9ad982ac5e7c297c8a42090e28e71984fb4c5918
DavoDev <juanpajmz13@gmail.com>
2026-01-26 18:31:02 -0600

    Modificar la impresión de información del primer repositorio para mostrar los primeros cinco repositorios en lugar de solo uno.

--- Books/Python/PythonCrashCourse/workingwapis/python_repos.py
+++ Books/Python/PythonCrashCourse/workingwapis/python_repos.py
@@ -19,12 +19,13 @@ repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
 # Examine the first repository
-repo_dict = repo_dicts[0]
-print("\n Selecting information about the first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository URL: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+# repo_dict = repo_dicts[0]s
+for repo_dict in repo_dicts:
+    print("\n Selecting information about the first repository:")
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository URL: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
