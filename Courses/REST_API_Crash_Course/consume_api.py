16f078e08f8d1426f102b9e02c240c97e37c88f4
DavoDev-Hub <juanpajmz13@gmail.com>
2026-02-01 20:00:53 -0600

    Agregar lógica para mostrar solo preguntas sin respuestas en la API de Stack Exchange

--- Courses/REST_API_Crash_Course/consume_api.py
+++ Courses/REST_API_Crash_Course/consume_api.py
@@ -3,6 +3,10 @@ import requests, json
 response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
 
 for data in response.json()['items']:
-    print(data['title'])
-    print(data['link'])
+    if data['answer_count'] == 0:
+        print(data['title'])
+        print(data['link'])
+        print()
+    else:
+        print("skipped")
     print()
