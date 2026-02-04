f103b5c79e159b767b64851456fd712dbbc3cba1
DavoDev-Hub <juanpajmz13@gmail.com>
2026-02-03 19:13:54 -0600

    Agregar funcionalidad para eliminar películas, incluyendo vista y ruta correspondiente

--- Courses/Django_Crash_Course/movies/urls.py
+++ Courses/Django_Crash_Course/movies/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', views.movies),
     path('movies/<int:id>/', views.detail),
-    path('movies/add', views.add)
+    path('movies/add', views.add),
+    path('movies/delete/<int:id>', views.delete),
 ]
